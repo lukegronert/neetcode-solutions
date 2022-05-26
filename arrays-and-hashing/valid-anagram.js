0d15c508d9ccc6c7334077eae20920a7e3e1770d
Luke Gronert <lukegronert@gmail.com>
2022-05-26 12:00:27 +0800

    Submitted different solution that results in less memory usage

--- arrays-and-hashing/valid-anagram.js
+++ arrays-and-hashing/valid-anagram.js
@@ -22,3 +22,37 @@
     t = t.join('')
     return s === t ? true : false
 };
+
+//Solution that uses less memory
+
+ var isAnagram = function(s, t) {
+    //If the strings are not the same length, they can't be anagrams
+    if(s.length !== t.length) {
+        return false;
+    }
+    let sHash = {};
+    let tHash = {};
+    //Set up hashMap for each string, counting each letter
+    for(let i = 0; i<s.length; i++) {
+        if(sHash[s[i]]) {
+            sHash[s[i]]+=1;
+        } else {
+            sHash[s[i]] = 1;
+        }
+    }
+    for(let j = 0;j<t.length;j++) {
+        if(tHash[t[j]]) {
+            tHash[t[j]]+=1;
+        } else {
+            tHash[t[j]]=1;
+        }
+    }
+    //check the value of each key in both hashmaps
+    for(key in sHash) {
+        //if they are not equal, they are not anagrams
+        if(sHash[key] !== tHash[key]) {
+            return false
+        }
+    }
+    return true;  
+};
