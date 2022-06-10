0e8ef0e4effe65c7f4db6f68d5333ddd3d358359
Luke Gronert <lukegronert@gmail.com>
2022-06-11 01:18:54 +0800

    Completed Two Sum II two pointer solution

--- two-pointers/two-sum-II.js
+++ two-pointers/two-sum-II.js
@@ -39,3 +39,18 @@
         }
     }
 };
+
+//Two pointer solution (slower, same memory)
+var twoSum = function(numbers, target) {
+    let l = 0;
+    let r = numbers.length-1
+    while(l<r) {
+        if(numbers[l] + numbers[r] > target) {
+            r--
+        } else if (numbers[l] + numbers[r] < target) {
+            l++
+        } else {
+            return [l+1,r+1]
+        }
+    }
+};
