63a8ab2553ce8a9fd7587495f71401bd1a9e7bde
Luke Gronert <lukegronert@gmail.com>
2022-06-14 23:40:56 +0800

    Completed Longest Substring Without Repeating

--- sliding-window/best-time-to-buy-and-sell-stock.js
+++ sliding-window/best-time-to-buy-and-sell-stock.js
@@ -36,3 +36,23 @@
     }
     return profit
 };
+
+// Faster, more memory efficient solution
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let l = 0;
+    let r = 1;
+    let profit = 0;
+    while(r<prices.length) {
+        if(prices[l] < prices[r]) {
+            profit = Math.max(profit,prices[r] - prices[l]);
+        } else {
+            l=r
+        }
+        r++
+    }
+    return profit
+};
