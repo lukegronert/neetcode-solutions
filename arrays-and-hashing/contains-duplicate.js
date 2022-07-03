d1c624c6557372ab13cfd7ebea6f743ee19029fa
lukegronert@gmail.com <lukegronert@gmail.com>
2022-07-03 00:37:21 -0700

    Redid contains duplicates 1 month later

--- arrays-and-hashing/contains-duplicate.js
+++ arrays-and-hashing/contains-duplicate.js
@@ -43,3 +43,15 @@ var containsDuplicate = function(nums) {
     }
     return false
 };
+
+// one month later
+var containsDuplicate = function(nums) {
+    let set = new Set();
+    for(let i=0; i<nums.length;i++) {
+        set.add(nums[i])
+    }
+    if(set.size < nums.length) {
+        return true
+    }
+    return false;
+};
