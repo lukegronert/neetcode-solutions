d8eb41b9238b965cf7b554da7bd6aeaebf7b4688
Luke Gronert <lukegronert@gmail.com>
2022-07-06 07:38:39 -0700

    one month later group anagrams

--- arrays-and-hashing/group-anagrams.js
+++ arrays-and-hashing/group-anagrams.js
@@ -68,3 +68,21 @@ var groupAnagrams = function(strs) {
     }
     return results
 };
+
+// one month later
+var groupAnagrams = function(strs) {
+    let map = {};
+    let result = [];
+    for(let word of strs) {
+        let key = word.split('').sort().join('');
+        if(map[key]) {
+            map[key].push(word)
+        } else {
+            map[key] = [word]
+        }
+    }
+    for(let key in map) {
+        result.push(map[key]);
+    }
+    return result;
+};
