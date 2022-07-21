b47d0867a524912acef3906c24717dc757ddda33
Luke Gronert <lukegronert@gmail.com>
2022-07-21 03:41:19 -0700

    Completed valid palindrome one month later

--- two-pointers/valid-palindromes.js
+++ two-pointers/valid-palindromes.js
@@ -78,3 +78,28 @@ var isPalindrome = function(s) {
     }
     return true
 };
+
+//one month later
+var isPalindrome = function(s) {
+    s=s.toLowerCase();
+    const isValidChar = (char) => {
+        let validChar = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        return validChar.includes(char);
+    };
+    let p1=0
+    let p2=s.length-1;
+    while(p1<p2) {
+        while(!isValidChar(s[p1]) && p1<p2) {
+            p1++
+        }
+        while(!isValidChar(s[p2]) && p1<p2) {
+            p2--
+        }
+        if(s[p1] !== s[p2]) {
+            return false;
+        }
+        p1++
+        p2--
+    }
+    return true
+};
